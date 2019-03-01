87ba3436389815b50fe4b9fd97d205e34c4b954a
Eber Romero <46845665+eberromero@users.noreply.github.com>
2019-03-01 00:34:01 -0300

    Commit inicial

---
+++ Desktop/Desktop.dpr
@@ -0,0 +1,14 @@
+program Desktop;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
